eae334b1b5507723da8b66ceb8e37f74a5a24ed7
Carlos Domínguez García <carlosdg97@gmail.com>
2018-08-29 10:01:58 +0100

    Refactor: using hidden canvas in DownloadableImage
    - Using hidden canvas so the user downloads the image exactly how we want it

--- src/js/components/DownloadableImage.js
+++ src/js/components/DownloadableImage.js
@@ -2,7 +2,11 @@ import React from "react";
 import DownloadLink from "./DownloadLink";
 
 /**
- * Image with a download link
+ * Image with a download link.
+ * Internally we use a hidden canvas and draw the image there. Then,
+ * when the user wants to download the image we use the data URL from
+ * the canvas. This way we make sure that the image downloaded have the
+ * dimensions that were demanded by parent components
  */
 export default class DownloadableImage extends React.Component {
   state = {
@@ -12,12 +16,19 @@ export default class DownloadableImage extends React.Component {
   render() {
     return (
       <React.Fragment>
+        <canvas
+          width={this.props.width}
+          height={this.props.height}
+          ref={canvas => (this.canvas = canvas)}
+          hidden
+        />
         <img
           src={this.props.dataUrl}
           alt={this.props.alt}
           width={this.props.width}
           height={this.props.height}
           onLoad={this.onImageLoad}
+          ref={image => (this.image = image)}
         />
         {this.getDownloadButton()}
       </React.Fragment>
@@ -33,7 +44,7 @@ export default class DownloadableImage extends React.Component {
       return (
         <DownloadLink
           downloadedFileName={this.props.imageName}
-          urlToDownload={this.props.dataUrl}
+          urlToDownload={this.canvas.toDataURL()}
         >
           {`Download ${this.props.imageName}`}
         </DownloadLink>
@@ -44,7 +55,14 @@ export default class DownloadableImage extends React.Component {
   }
 
   /**
-   * Sets the component state to indicate that the image has been loaded
+   * Load the image in the canvas. And set the component state
+   * to indicate that the image has been loaded
    */
-  onImageLoad = () => this.setState({isImageLoaded: true})
+  onImageLoad = () => {
+    this.canvas
+      .getContext("2d")
+      .drawImage(this.image, 0, 0, this.props.width, this.props.height);
+
+    this.setState({ isImageLoaded: true });
+  };
 }
