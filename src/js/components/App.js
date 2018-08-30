f1e54b788451cf35dd3e27ff6189f5d07f5abd88
Carlos Domínguez García <carlosdg97@gmail.com>
2018-08-30 10:17:12 +0100

    Style & Refactor:
    - Added basic styles using className
    - Added componentDidMount to the App component to initialize there the screamStylePromise

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -8,31 +8,44 @@ import {
 } from "../utils/dimensions";
 import * as MediaUtils from "../utils/mediaUtils";
 
-// Loads the model used to transfer the Screan paint style
-const screamStylePromise = ml5.styleTransfer("models/scream");
-
 export default class App extends React.Component {
-  /**
-   * Reference to the camera component instance
-   */
-  camera = null;
+  constructor(args) {
+    super(...args);
 
-  state = {
-    imageDataUrls: []
-  };
+    this.state = {
+      imageDataUrls: []
+    };
+
+    // Reference to the camera component instance
+    this.camera = null;
+  
+    // Promise used for transfering the Scream style to photos.
+    // Initialized when component mounts
+    this.screamStylePromise = null;
+  }
+
+  componentDidMount() {
+    // Loads the model used to transfer the Scream paint style
+    this.screamStylePromise = ml5.styleTransfer("models/scream");
+  }
 
   render() {
     return (
-      <div>
+      <div className="centered">
         <Camera
           width={MAX_DIMENSIONS.width}
           height={MAX_DIMENSIONS.height}
           photoWidth={MAX_DIMENSIONS.width}
           photoHeight={MAX_DIMENSIONS.height}
           ref={camera => (this.camera = camera)}
+          className="container"
         />
-        <button onClick={this._onNewPhoto}>Take photo</button>
-        <ul>{this.state.imageDataUrls.map(this._createPhotoListItem)}</ul>
+        <button onClick={this._onNewPhoto} className="button">
+          Take photo
+        </button>
+        <ul className="result-list container">
+          {this.state.imageDataUrls.map(this._createPhotoListItem)}
+        </ul>
       </div>
     );
   }
@@ -42,7 +55,7 @@ export default class App extends React.Component {
    * Callback to run when the user wants to take a photo
    */
   _onNewPhoto = () => {
-    screamStylePromise
+    this.screamStylePromise
       .then(style => style.transfer(this.camera.video))
       .then(result =>
         this.setState(prevState => ({
@@ -61,7 +74,7 @@ export default class App extends React.Component {
     const dimensions = getMaxDimensionsRespectingAspectRatio(cameraAspectRatio);
 
     return (
-      <li key={index}>
+      <li key={index} className="result-container">
         <DownloadableImage
           dataUrl={dataUrl}
           alt={`Photo ${index}`}
