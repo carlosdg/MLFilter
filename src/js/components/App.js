02d9b03293bf2ffb484c75f7c082ee1f0ecb04bf
Carlos Domínguez García <carlosdg97@gmail.com>
2018-08-30 11:06:53 +0100

    Fix: photos changing dimensions
    - When users made a photo, all the photos were rerendered and thus the aspect ratio used for computing their dimensions were not the same as the one when the photos were made. This could be seen on mobiles when the users rotate the mobile and made a new photo. To solve it we moved the calculation of the dimensions out of the view and to the model, saving them as part of the state after transfering the style

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -13,12 +13,12 @@ export default class App extends React.Component {
     super(...args);
 
     this.state = {
-      imageDataUrls: []
+      imageData: []
     };
 
     // Reference to the camera component instance
     this.camera = null;
-  
+
     // Promise used for transfering the Scream style to photos.
     // Initialized when component mounts
     this.screamStylePromise = null;
@@ -44,7 +44,7 @@ export default class App extends React.Component {
           Take photo
         </button>
         <ul className="result-list container">
-          {this.state.imageDataUrls.map(this._createPhotoListItem)}
+          {this.state.imageData.map(this._createPhotoListItem)}
         </ul>
       </div>
     );
@@ -52,16 +52,26 @@ export default class App extends React.Component {
 
   /**
    * [Private method]
-   * Callback to run when the user wants to take a photo
+   * Callback to run when the user wants to take a photo:
+   * gets the current frame on the camera, applies the style
+   * transer and updates the component state
    */
   _onNewPhoto = () => {
     this.screamStylePromise
       .then(style => style.transfer(this.camera.video))
-      .then(result =>
+      .then(result => {
+        const cameraAspectRatio = MediaUtils.getAspectRatio(this.camera.video);
+        const dimensions = getMaxDimensionsRespectingAspectRatio(cameraAspectRatio);
+        const newImageData = {
+          dataUrl: result.src,
+          width: dimensions.width,
+          height: dimensions.height,
+        }
+
         this.setState(prevState => ({
-          imageDataUrls: [...prevState.imageDataUrls, result.src]
-        }))
-      );
+          imageData: [...prevState.imageData, newImageData]
+        }));
+      });
   };
 
   /**
@@ -69,17 +79,14 @@ export default class App extends React.Component {
    * Returns a list item component to show a new downloadable image which
    * respects the aspect ratio of the camera
    */
-  _createPhotoListItem = (dataUrl, index) => {
-    const cameraAspectRatio = MediaUtils.getAspectRatio(this.camera.video);
-    const dimensions = getMaxDimensionsRespectingAspectRatio(cameraAspectRatio);
-
+  _createPhotoListItem = (imageData, index) => {
     return (
       <li key={index} className="result-container">
         <DownloadableImage
-          dataUrl={dataUrl}
+          dataUrl={imageData.dataUrl}
           alt={`Photo ${index}`}
-          width={dimensions.width}
-          height={dimensions.height}
+          width={imageData.width}
+          height={imageData.height}
           imageName={`Photo ${index}`}
         />
       </li>
